e336b7bba82006f6d30bc19d698e2c9a34e57e16
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-01-12 12:34:59 -0500

    Pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+// SET UP VARIABLES
+//=======================================================
+// SEARCH PARAMETERS
+// BASE URL
+ let queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=44a6ad4a174b6c522a94ebe5df83deda&units=imperial"
+
+// VARIABLE TO TRACK CITIES
+// FUNCTIONS
+//=======================================================
+// MAIN PROCESS
+//=======================================================
+
+// 1. Retrive user input, convert into variables
+// 2. Use variables to run an AJAX call to Open Weather
+// 3. Break down the Open Weather Object into usable fields. 
+// 4. Dynamically generate HTML content
+
+// 5. Deal with "edge cases".
+
+});
