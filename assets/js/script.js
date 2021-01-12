20bf6893f9330c92516e14235436a73ed40fce4c
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-01-12 16:44:37 -0500

    search button functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,36 @@
 $(document).ready(function() {
 // SET UP VARIABLES
 //=======================================================
-// SEARCH PARAMETERS
-// BASE URL
- let queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=44a6ad4a174b6c522a94ebe5df83deda&units=imperial"
 
-// VARIABLE TO TRACK CITIES
+// Search Parameters
+// Base URL
+//  let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=44a6ad4a174b6c522a94ebe5df83deda&units=imperial";
+//  console.log(queryURL);
+
+// VARIABLE FOR FORECAST
+let dayForecast = 0;
 // FUNCTIONS
 //=======================================================
+
+
+$("#search-button").click(function() {
+    var citySearch = $('#search-city').val();
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=44a6ad4a174b6c522a94ebe5df83deda&units=imperial";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(WeatherData){
+        console.log(WeatherData);
+    })
+
+})
+
 // MAIN PROCESS
 //=======================================================
+
+
 
 // 1. Retrive user input, convert into variables
 // 2. Use variables to run an AJAX call to Open Weather
