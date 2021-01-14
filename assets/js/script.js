f5620e8a81259c66f9d660b4754e497e765a01bd
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-01-14 14:32:10 -0500

    Current weather info displays in DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,27 @@ let history = [];
 // FUNCTIONS
 //=======================================================
 
-
+// Search Button
+//=======================================================
 $("#search-button").on("click", function() {
     var search = $("#search-city").val();
 
+    $("#search-city").empty();
+
     getWeather(search);
 });
-
+// Display searched cities to toggle back and forth
+//=======================================================
 $(".history").on("click", "li", function() {
     getWeather($(this).text());
 });
 
-function row(history) {
-    var li = $("<li>").addClass("list-group-item list-group-action").text(history);
+function row(city) {
+    var li = $("<li>").addClass("list-group-item").text(city);
     $(".history").append(li);
 }
-
+// Retrieve weather data
+//=======================================================
 function getWeather(search) {
     $.ajax({
         type: "GET",
@@ -38,23 +43,39 @@ function getWeather(search) {
 
             console.log("https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=44a6ad4a174b6c522a94ebe5df83deda&units=imperial");
             console.log(data);
-
+// Search history
+//=======================================================
             if (history.indexOf(search) === -1) {
                 history.push(search);
                 window.localStorage.setItem("history", JSON.stringify(history));
 
                 row(history);
             }
+
+// Create and append elements to DOM
+//=======================================================
+
+            let cityDate = $("<h2>").addClass("city-date").text(data.name + new Date().toLocaleDateString());
+            // $("div #today").append("<h2>" + data.name + new Date().toLocaleDateString() + "</h2>"); 
+            let temp = $("<p>").text("Current Temperature: " + data.main.temp + "°F");
+            let humidity = $("<p>").text("Humidity: " + data.main.humidity + "%");
+            let windSpeed = $("<p>").text("Wind Speed: " + data.wind.speed + "MPH");
+            // console.log(data.name);
+            // console.log(new Date().toLocaleDateString());
+            // console.log("temp: " + data.main.temp);
+            // console.log("humidity: " + data.main.humidity);
+            // console.log("wind speed: " + data.wind.speed);
+            $("#today").append(cityDate, temp, humidity, windSpeed);
+            
+        
+            
         }
     })
-}
+};
 
 
-// 1. Retrive user input, convert into variables
-// 2. Use variables to run an AJAX call to Open Weather
-// 3. Break down the Open Weather Object into usable fields. 
-// 4. Dynamically generate HTML content
 
-// 5. Deal with "edge cases".
+
+
 
 });
